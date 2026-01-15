d90489d14c5f6ddf6b441a10e763341a1497fe49
moriy <moriyc@post.bgu.ac.il>
2026-01-15 02:59:09 +0200

    update

--- search_frontend.py
+++ search_frontend.py
@@ -314,4 +314,4 @@ def run(**options):
 
 if __name__ == '__main__':
     # run the Flask RESTful API, make the server publicly available (host='0.0.0.0') on port 8080
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
